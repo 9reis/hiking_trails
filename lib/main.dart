93ad0da788747ac6abe95f89e7d91aeb7e83ba6a
Lucas Reis <lucasreis_9@hotmail.com>
2023-10-13 17:07:54 -0300

    Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hiking_trails/utils/constants.dart';
 
-void main() =>
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // Status bar 
+      statusBarBrightness: Brightness.dark, // Icones da Status bar
+    ),
+  );
   runApp(const MyApp());
-
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,13 +18,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          primary: kPrimaryColor,
+        ),
         useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ,
+      home: Container(
+        color: Colors.green,
+      ),
     );
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0XFF202020);
+const kAccentColor = Color(0XFFA7B0C1);
+const kButtonColor = Color(0XFF2A8188);
+const kRattingColor = Color(0XFFFBD746);
+const kLightColor = Color(0XFFFFFFFF);
